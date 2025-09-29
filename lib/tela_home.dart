f70e761a17ba0c05645297c8c9e4be0087990d17
pedlinn <pg.pessoall@gmail.com>
2025-09-29 11:42:00 -0300

    inserção cadastro de restaurante

--- lib/tela_home.dart
+++ lib/tela_home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:projetosflutter/tela_cad_restaurante.dart';
 
 class TelaHome extends StatelessWidget{
   TelaHome({super.key});
@@ -7,8 +8,31 @@ class TelaHome extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Seja bem vindo Usuário: ")),
-      body: const Center(
-        child: Text("Tela de Home"),
+      body: Padding(padding: const EdgeInsets.all(80),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Lista de Restaurantes:"),
+            ElevatedButton(onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context) => TelaCadRestaurante()));
+            }, child: Text("Cadastrar Novo Restaurante")),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    Text("Meu restaurante"),
+                    Text("Comida Mexicana")
+                  ],
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(onPressed: (){}, child: Text("Excluir")),
+                SizedBox(width: 10),
+                ElevatedButton(onPressed: (){}, child: Text("Editar"))
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
